6fcdb4372746762595bf2ad40b924fa8aed3e3d7
AnandAK07 <anandkumark055@gmail.com>
2024-02-14 16:52:38 +0530

    deploy

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const connection=require('./config/db')
 const userRouter = require('./routes/user.route');
 const authentication = require('./middleware/authentication');
 const productRouter = require('./routes/product.route');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app=express();
 app.use(express.json())
@@ -15,10 +17,12 @@ app.use('/api', userRouter)
 app.use(authentication)
 app.use('/api',productRouter)
 
-app.listen(8080,async()=>{
+
+const PORT=process.env.PORT;
+app.listen(PORT,async()=>{
     try {
         await connection()
-        console.log(`8080`)
+        console.log(`${PORT}`)
     } catch (error) {
         console.log('failed to conect')
     }
